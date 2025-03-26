e597c4b4600ad7a409382defce9e06888f9b7e15
JrCooler <mehmetemir.uzun1312@gmail.com>
2025-03-26 22:14:42 +0100

    add shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { cn } from "$lib/utils"
+</script>
+
+<h1 class={cn("text-3xl", "text-center")}>Welcome to SvelteKit</h1>
+<p class="text-center">
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
