4f7a09d17c89e0d380767711e6d7b2c2cb2f634f
JrCooler <mehmetemir.uzun1312@gmail.com>
2025-03-27 17:24:43 +0100

    add drizzle-kit

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,10 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import {
+  pgTable,
+  serial,
+  text,
+  integer,
+  date,
+  jsonb,
+} from "drizzle-orm/pg-core"
 
-export const user = pgTable('user', {
-	id: serial('id').primaryKey(),
-	age: integer('age')
-});
+// Your schema here
